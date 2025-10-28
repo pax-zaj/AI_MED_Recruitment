2375526fa83754f44f3c045db6baf89836abc526
pax-zaj <karolinazajac2006@gmail.com>
2025-10-28 12:30:26 +0100

    Update cardiomegaly.py

--- cardiomegaly.py
+++ cardiomegaly.py
@@ -23,3 +23,38 @@ x = data[[
 y = data["Cardiomegaly"]
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+
+x_scaled_train = scaler.fit_transform(x_train)
+x_scaled_test = scaler.transform(x_test)
+
+#first method - KNN
+
+param_grid_knn = {
+    "n_neighbours": [3, 5, 7, 9, 11, 13, 15, 17, 19, 21],
+    "weights": ["uniform", "distance"],
+    "metric": ["euclidean", "manhattan", "minkowski", "chebyshev"]
+}
+
+rsfk = RepeatedStratifiedKFold(
+    n_splits = 5,
+    n_repeats = 100,
+    random_state = None
+)
+
+pipe_knn = Pipeline(steps=[
+    ("scaler", StandardScaler()),
+    ("model", KNeighborsClassifier())
+])
+
+grid_search = GridSearchCV(
+    estimator = pipe_knn,
+    param_grid = param_grid_knn,
+    scoring = "accuracy",
+    cv = rsfk,
+    verbose = 1,
+    n_jobs = -1
+)
+
+grid_search.fit(x_train, y_train)
